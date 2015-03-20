0731f53c7c2a0d6d553b9d6ff71d463f2fc90ee5
MuchContact <422549477@qq.com>
2015-03-20 10:54:19 +0800

    change the order of loading layers and make outputFormat with json

--- openlayers demos/ol2/examples/wfs-filter.js
+++ openlayers demos/ol2/examples/wfs-filter.js
@@ -5,44 +5,41 @@ OpenLayers.ProxyHost= "http://localhost:8090/CrossDomainProxy/cgi-bin/proxy.cgi?
 
 function init() {
 	
-    map = new OpenLayers.Map({
-        div: "map",
-        layers: [
-            new OpenLayers.Layer.WMS(
+    map = new OpenLayers.Map('map');
+	map.addLayer(new OpenLayers.Layer.WMS(
                 "Natural Earth", 
                 "http://demo.opengeo.org/geoserver/wms",
                 {layers: "maps:ne_50m_land"}
-            ),
-            new OpenLayers.Layer.Vector("WFS", {
-                strategies: [new OpenLayers.Strategy.BBOX()],
-                protocol: new OpenLayers.Protocol.WFS({
-                    url:  "http://localhost:8080/geoserver/wfs",
-                    featureType: "archsites",
-                    featureNS: "http://www.openplans.org/spearfish",
-					featurePrefix: "",
-					srsName: "EPSG:4326", // this is the default,
-					version: "1.1.0"
-                }),
-				filter: new OpenLayers.Filter.Logical({
-					type:	OpenLayers.Filter.Logical.AND,
-					filters:	[
-						new OpenLayers.Filter.Comparison({
-							type: OpenLayers.Filter.Comparison.GREATER_THAN,
-                            property: "cat",
-                            value: 20
-						}),
-						new OpenLayers.Filter.Comparison({
-							type: OpenLayers.Filter.Comparison.LESS_THAN,
-                            property: "cat",
-                            value: 50
-						})
-					]
+    ));
+	map.setCenter(new OpenLayers.LonLat(0, 0), 1);	
+	//strategies: [new OpenLayers.Strategy.TFS({resFactor: 1,ratio:1})],
+	var layer = new OpenLayers.Layer.Vector("WFS", {
+		strategies: [new OpenLayers.Strategy.BBOX()],
+		protocol: new OpenLayers.Protocol.WFS({
+			url:  "http://localhost:8080/geoserver/wfs",
+			featureType: "archsites",
+			featureNS: "http://www.openplans.org/spearfish",
+			featurePrefix: "",
+			srsName: "EPSG:4326", // this is the default,
+			version: "1.1.0",
+			outputFormat: "json"
+		}),
+		filter: new OpenLayers.Filter.Logical({
+			type:	OpenLayers.Filter.Logical.AND,
+			filters:	[
+				new OpenLayers.Filter.Comparison({
+					type: OpenLayers.Filter.Comparison.GREATER_THAN,
+					property: "cat",
+					value: 20
+				}),
+				new OpenLayers.Filter.Comparison({
+					type: OpenLayers.Filter.Comparison.LESS_THAN,
+					property: "cat",
+					value: 50
 				})
-            })
-			
-        ],
-        center: new OpenLayers.LonLat(1.7, 0.8),
-        zoom: 1
-    });
+			]
+		})
+	});
+	map.addLayer(layer);
 
 }
